dd9698d0314f082cf157a0f5318d0fe36698acda
Said <saidozcn@gmail.com>
2016-08-25 13:24:13 +0300

    update AppDelegate with Flickr API credentials handle

--- Harnosand/Common/AppDelegate.swift
+++ Harnosand/Common/AppDelegate.swift
@@ -18,6 +18,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         guard let window = window else { return false }
         
+        guard let credentialsPath = NSBundle.mainBundle().pathForResource("Credentials", ofType: "plist") else {
+            print("Credentials.plist not found")
+            return false
+        }
+        if let credentials = NSDictionary(contentsOfFile: credentialsPath) as? Dictionary<String, AnyObject>{
+            if let apiKey = credentials["apiKey"] as? String{
+                FlickrServiceRouter.apiKey = apiKey
+            }else{
+                print("Can't find key: 'apiKey' in Credentials.plist")
+            }
+        }else{
+            print("Can't read Credentials.plist as dictionary.")
+            return false
+        }
+        
         window.backgroundColor = UIColor.whiteColor()
         window.makeKeyAndVisible()
         
