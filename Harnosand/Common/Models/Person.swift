636addaaa75116b2c31f5e5bad55265e31500f88
Said <saidozcn@gmail.com>
2016-08-27 02:58:35 +0300

    update Person model with fallback user avatar property

--- Harnosand/Common/Models/Person.swift
+++ Harnosand/Common/Models/Person.swift
@@ -12,12 +12,16 @@ import Unbox
 struct Person: Unboxable{
     let identifier: String
     let nsid: String
-    let iconServer: String
+    let iconServer: Int
     let iconFarm: Int
     let username: String
     var profileImageURL: NSURL{
         get{
-            return NSURL(string:"https://farm\(self.iconFarm).staticflickr.com/\(self.iconServer)/buddyicons/\(self.nsid).jpg")!
+            if iconServer > 0{
+                return NSURL(string:"https://farm\(self.iconFarm).staticflickr.com/\(self.iconServer)/buddyicons/\(self.nsid).jpg")!
+            }else{
+                return NSURL(string:"https://www.flickr.com/images/buddyicon.gif")!
+            }
         }
     }
     
