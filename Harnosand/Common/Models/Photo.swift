2d6d01cce7832fc728dc2b9731c98e5fabbdc1a1
Said <saidozcn@gmail.com>
2016-08-27 01:41:45 +0300

    update Photo model with new image resolution and more optional fields

--- Harnosand/Common/Models/Photo.swift
+++ Harnosand/Common/Models/Photo.swift
@@ -10,23 +10,30 @@ import Foundation
 import Unbox
 
 struct Photo: Unboxable{
-    let identifier: String
-    let owner: String
-    let title: String
+    let identifier: String?
+    let owner: String?
+    let title: String?
     let dateTaken: NSDate?
-    let media: String
+    let media: String?
     let url: NSURL?
+    let width: Float?
+    let height: Float?
     
     init(unboxer: Unboxer) {
         self.identifier = unboxer.unbox("id")
         self.owner = unboxer.unbox("owner")
         self.title = unboxer.unbox("title")
         self.media = unboxer.unbox("media")
-        if let url:String = unboxer.unbox("url_l"){
+        
+        if let url:String = unboxer.unbox("url_c"){
             self.url = NSURL(string: url)
         }else{
             self.url = nil
         }
+        
+        self.width = unboxer.unbox("width_c")
+        self.height = unboxer.unbox("height_c")
+        
         self.dateTaken = unboxer.unbox("datetaken", formatter:CachedDateFormatter.sharedInstance.formatterWith("YYYY-MM-dd HH:mm:ss"))
     }
 }
